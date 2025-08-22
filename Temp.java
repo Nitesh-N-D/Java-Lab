04a18f9de53829094fd2a933bfb7637a7cab998b
Nitesh N D <niteshdwaraka@gmail.com>
2025-08-22 23:22:05 +0530

    Update Temp.java

--- Temp.java
+++ Temp.java
@@ -1,1 +1,18 @@
+import java.util.Scanner;
+class Temp
+{ 
+    public static void main(String[] args)
+         {
+            double c,f;
+            boolean hot;
+            Scanner sc=new Scanner(System.in);
+            System.out.println("Name:N.D.Nitesh \nRoll.No:2024503529");
+            System.out.println("Enter the temperature(in Celsius):");
+            c=sc.nextDouble();
+            f=(c*9/5)+32;
+            hot=f>100;
+            System.out.println("Temperature:"+c+"C"+"("+f+"F)");
+            System.out.println("Hot:"+hot);
+          }
+}
 
