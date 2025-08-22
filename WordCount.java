390544405b0931cd9577023654803f3dba312194
Nitesh N D <niteshdwaraka@gmail.com>
2025-08-22 23:39:02 +0530

    Update WordCount.java

--- WordCount.java
+++ WordCount.java
@@ -1,1 +1,39 @@
+import java.util.Scanner;
 
+public class WordCount {
+
+public static void main(String[] args)
+
+{
+
+Scanner sc = new Scanner(System.in);
+
+System.out.print("Enter a string:");
+
+String str=sc.nextLine();
+
+String[] words=str.split(" ");
+
+int count = 0;
+
+for (String word:words)
+
+{
+
+if (!word.isEmpty())
+
+{
+
+count++;
+
+}
+
+}
+
+System.out.println("Number of words:" +count);
+
+sc.close();
+
+}
+
+}
