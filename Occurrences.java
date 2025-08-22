e5cb05ea797de40cfbc7b1d67a7b8f7b6e436332
Nitesh N D <niteshdwaraka@gmail.com>
2025-08-22 23:34:18 +0530

    Update Occurrences.java

--- Occurrences.java
+++ Occurrences.java
@@ -1,1 +1,36 @@
 
+import java.util.Scanner;
+
+import java.util.HashMap;
+
+public class Occurrences {
+
+public static void main(String[] args)
+
+{
+
+Scanner sc=new Scanner(System.in);
+
+String str;
+
+System.out.println("Enter the string:");
+
+str=sc.nextLine();
+
+HashMap<Character,Integer>map=new HashMap<>();
+
+for(char c:str.toCharArray())
+
+{
+
+map.put(c,map.getOrDefault(c,0)+1);
+
+}
+
+System.out.println("The number of occurrences is:");
+
+System.out.println(map);
+
+}
+
+}
