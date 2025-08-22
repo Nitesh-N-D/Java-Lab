608c8daa8770dbd265b45251563f79169bf16370
Nitesh N D <niteshdwaraka@gmail.com>
2025-08-22 23:25:28 +0530

    Update SingleInheritance.java

--- SingleInheritance.java
+++ SingleInheritance.java
@@ -1,1 +1,75 @@
 
+class LivingBeing {
+
+LivingBeing()
+
+{
+
+System.out.println("LivingBeing constructor called");
+
+}
+
+void Breath() {
+
+System.out.println("They are breathing");
+
+}
+
+void Response()
+
+{
+
+System.out.println("They are responsing");
+
+}
+
+
+}
+
+class Animal extends LivingBeing
+
+{
+
+Animal()
+
+{
+
+System.out.println("Animal constructor called");
+
+}
+
+void Walk()
+
+{
+
+System.out.println("Animals are walking");
+
+}
+
+void NoOfLegs()
+
+{
+
+System.out.println("Animals have 4 legs");
+
+}
+
+}
+
+public class SingleInheritance {
+
+public static void main(String[] args) {
+
+Animal a=new Animal();
+
+a.Breath();
+
+a.Response();
+
+a.Walk();
+
+a.NoOfLegs();
+
+}
+
+}
