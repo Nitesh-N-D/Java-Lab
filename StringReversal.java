3a114b9402dbe115e36ce7c65800e06d1b98a939
Nitesh N D <niteshdwaraka@gmail.com>
2025-08-22 23:33:06 +0530

    Update StringReversal.java

--- StringReversal.java
+++ StringReversal.java
@@ -1,1 +1,41 @@
+import java.util.Scanner;
 
+public class StringReversal {
+
+public static void main(String[] args)
+
+{
+
+String str;
+
+Scanner sc=new Scanner(System.in);
+
+System.out.println("Enter the string to reverse:");
+
+str=sc.nextLine();
+
+int n=str.length();
+
+char[] c=str.toCharArray();
+
+for(int i=0;i<n/2;i++)
+
+{
+
+int j=n-i-1;
+
+char temp=c[i];
+
+c[i]=c[j];
+
+c[j]=temp;
+
+}
+
+System.out.println("The reversed string is:");
+
+System.out.println(c);
+
+}
+
+}
