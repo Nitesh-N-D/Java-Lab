3129cfe6b3b29c30744585f58dbf0446096bc060
Nitesh N D <niteshdwaraka@gmail.com>
2025-08-22 23:43:36 +0530

    Update StringCompression.java

--- StringCompression.java
+++ StringCompression.java
@@ -1,1 +1,49 @@
+import java.util.Scanner;
 
+public class StringCompression
+
+{
+
+public static void main(String[] args) {
+
+Scanner sc=new Scanner(System.in);
+
+System.out.print("Enter a string:");
+
+String str = sc.nextLine();
+
+StringBuilder compressed = new StringBuilder();
+
+int count = 1;
+
+for(int i=0;i<str.length();i++)
+
+{
+
+if(i+1<str.length()&&str.charAt(i)==str.charAt(i+1))
+
+{
+
+count++;
+
+}
+
+else
+
+{
+
+compressed.append(str.charAt(i)).append(count);
+
+count = 1;
+
+}
+
+}
+
+System.out.println("The Compressed string is:"+(compressed.length()<str.length()?compressed:str));
+
+sc.close();
+
+}
+
+}
