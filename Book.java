5c4f6e4a5d1a237088414e3170022d6e0153899a
Nitesh N D <niteshdwaraka@gmail.com>
2025-08-30 00:16:19 +0530

    Create  Book.java
    This program demonstrates constructor chaining in Java.
    The Book class has three constructors (default, two-parameter, and three-parameter). Each calls another using this(...), reducing redundancy. The program shows how different constructors initialize Book objects.

---
+++ Book.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+class Book
+{
+    String title;
+    String author;
+    double price;
+    Book()
+    {
+        this("Unknown title","Unknown author");
+        System.out.println("Default constructor called");
+    }
+    Book(String title,String author)
+    {
+        this(title,author,0.0);
+        System.out.println("With 2 parameters");
+    }
+    Book(String title,String author,double price)
+    {
+        this.title=title;
+        this.author=author;
+        this.price=price;
+        System.out.println("3 parameters");
+    }
+    void display()
+    {
+        System.out.println("Title:"+title);
+        System.out.println("Author:"+author);
+        System.out.println("Price:"+price);
+    }
+}
+    public class Chain {
+        public static void main(String[] args) {
+            Scanner sc=new Scanner(System.in);
+            Book b1=new Book();
+            b1.display();
+            Book b2=new Book("Wings of fire","APJ");
+            b2.display();
+            Book b3=new Book("Java","Robert",550);
+            b3.display();
+            sc.close();
+        }
+    
+}
