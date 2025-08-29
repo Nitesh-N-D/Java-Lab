7dfa6cc86ab5d8d46e45eb4b7df37d5798dfc198
Nitesh N D <niteshdwaraka@gmail.com>
2025-08-29 23:39:17 +0530

    Create  Eligibility.java
    This Java program checks vaccination eligibility for patients based on age and appointment number. It first asks for the number of patients, then for each patient, verifies if their age lies between 18 and 65. Eligible patients must enter a valid 3-digit appointment number. Depending on the number, they are assigned to either a priority slot (even) or a regular slot (odd). Based on the last digit, patients may also receive a gift such as a pen, mask, or sanitizer. The program ensures clear validation, structured output, and demonstrates Java concepts like loops, conditions, and switch statements.

---
+++ Eligibility.java
@@ -0,0 +1,107 @@
+import java.util.Scanner;
+
+public class Eligibility {
+
+public static void main(String[] args)
+
+{
+
+Scanner sc=new Scanner(System.in);
+
+System.out.println("Enter the no.of patients:");
+
+int no=sc.nextInt();
+
+for(int i=1;i<=no;i++)
+
+{
+
+System.out.println("Patient:"+i);
+
+System.out.println("Enter age:");
+
+int age=sc.nextInt();
+
+if(age>=18&&age<=65)
+
+{
+
+System.out.println("Eligible for vaccination");
+
+System.out.println("Enter 3-digit appt number:");
+
+int apptno=sc.nextInt();
+
+if(apptno>=100 && apptno<=999)
+
+{
+
+if(apptno%2==0)
+
+{
+
+System.out.println("Assigned to priority slot");
+
+}
+
+else
+
+{
+
+System.out.println("Assigned to regular slot");
+
+}
+
+int ldigit=apptno%10;
+
+switch(ldigit)
+
+{
+
+case 1:
+
+System.out.println("Gift:Pen");
+
+break;
+
+case 2:
+
+System.out.println("Gift:Mask");
+
+break;
+
+case 3:
+
+System.out.println("Gift:Sanitizer");
+
+break;
+
+default:
+
+System.out.println("No special gifts");
+
+}
+
+}
+
+else
+
+{
+
+System.out.println("Invalid appointment number");
+
+}
+
+}
+
+else
+
+{
+
+System.out.println("Not eligible for vaccination");
+
+} 
+}
+sc.close();
+}
+}
